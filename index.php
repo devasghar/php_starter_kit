5864cfb224af1310ea60445e345071a095b51422
devasghar <dev.asghar@gmail.com>
2017-11-06 00:06:37 +0500

    blank php project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Web Elements</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="dist/styles/styles.min.css">
+    <script type="application/javascript" src="dist/scripts/main.min.js"></script>
+</head>
+
+<body>
+  
+</body>
+
+</html>
